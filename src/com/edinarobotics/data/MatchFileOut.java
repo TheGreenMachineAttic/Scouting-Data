6e66353fcd85381328cc3d8986179554a9b8c5c9
Alex O <alexander4913@aol.com>
2012-03-20 16:56:38 -0500

    Started cleaning up some messy code

---
+++ src/com/edinarobotics/data/MatchFileOut.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.edinarobotics.data;
+/*
+ * @author aoneill
+ * @breif
+ */
+
+public class MatchFileOut 
+{
+    public MatchFileOut()
+    {
+
+    }
+}
